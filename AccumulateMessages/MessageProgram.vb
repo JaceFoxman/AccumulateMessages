1d69124f3eeadcdd2dc70acd928c270cac81b875
Jason Permann <JaceFoxman@gmail.com>
2025-02-16 15:53:28 -0700

    Add text builder

--- AccumulateMessages/MessageProgram.vb
+++ AccumulateMessages/MessageProgram.vb
@@ -1,5 +1,6 @@
 
 Imports System
+Imports System.Net.Security
 
 Module MessageProgram
     Sub Main(args As String())
@@ -8,8 +9,18 @@ Module MessageProgram
         Test.Auto()
     End Sub
 
+
+
+    'Use this function to write what Test program needs, example: when test program gives clear true/false run through if statement
     Function UserMessages(ByVal newMessage As String, ByVal clear As Boolean) As String
-        'your code here
+
+        'brings test data as array from Test program, this allows the exact string needed to be written in the code to appear. 
+        Static messages As New Text.StringBuilder()
+
+        If  Then
+
+        End If
+
         Return messages
     End Function
 
